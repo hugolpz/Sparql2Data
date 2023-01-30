91b3f0ee1dd28734f8e5a46877c28b673da2147f
Lopez Hugo <hugo.lpz@gmail.com>
2023-01-30 22:25:58 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@
 query=$(cat languages.sparql)
 
 # Query Wikidata with SPARQL
-response=$(curl -G --data-urlencode 'query=$query' https://lingualibre.org/sparql?format=json) | jq '.results.bindings[]'
+response=$(curl -G --data-urlencode 'query=${query}' https://lingualibre.org/sparql?format=json) | jq '.results.bindings[]'
 # Save to file
 echo "$reponse" > languages.json
 
