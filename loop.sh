eb74ba3b3d4c3edf3a81fc58c1e1389b29865704
hugolpz <hugo.lpz@gmail.com>
2023-02-02 21:53:19 +0100

    fix script name

--- loop.sh
+++ loop.sh
@@ -3,9 +3,9 @@ targetsLL=$(ls -1 ./sparql/LL-L*)
 targetsWD=$(ls -1 ./sparql/WD-*)
 
 for file in $targetsLL; do
-  test.sh -q $file --service=lingualibre -format json
+  bash ./script.sh -q $file --service=lingualibre -format json
 done
 
 for file in $targetsWD; do
-  test.sh -q $file --service=wikidata -format json
+  bash ./script.sh -q $file --service=wikidata -format json
 done
