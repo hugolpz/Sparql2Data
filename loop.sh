3b6ae33f9fb1893e9cb7ba28581d09413e634beb
Lopez Hugo <hugo.lpz@gmail.com>
2023-02-04 17:29:18 +0100

    Prioritize LL-

--- loop.sh
+++ loop.sh
@@ -1,7 +1,11 @@
 ## Targets
 targetsWD=$(ls -1 ./sparql/WD-*)
 targetsWC=$(ls -1 ./sparql/WC-*)
-targetsLL=$(ls -1 ./sparql/LL-*)
+targetsLL=$(ls -1 ./sparql/LL-e)
+
+for file in $targetsLL; do
+  bash ./script.sh --sparql $file --service lingualibre --format json 
+done
 
 for file in $targetsWD; do
   bash ./script.sh --sparql $file --service wikidata --format json 
@@ -9,8 +13,4 @@ done
 
 for file in $targetsWC; do
   bash ./script.sh --sparql $file --service commons --format json 
-done
-
-for file in $targetsLL; do
-  bash ./script.sh --sparql $file --service lingualibre --format json 
 done
