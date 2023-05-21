7f2eaec7c171def3e3a4ec71a4f26b8b9a7f399b
Lopez Hugo <hugo.lpz@gmail.com>
2023-05-21 22:12:28 +0200

    Sort by descreasing based on .records

--- languages-gallery.sh
+++ languages-gallery.sh
@@ -1,3 +1,3 @@
-jq -s 'add | group_by(.wikidata) | map(add) | map(select(.records > 0))' \
+jq -s 'add | group_by(.wikidata) | map(add) | map(select(.records > 0)) | sort_by(.records) | reverse' \
     ./data/LL-LanguagesSpeakersData.json ./data/LL-LanguagesGenderData.json ./data/LL-LanguagesActive.json ./data/LL-LanguagesRecordsData.json ./data/WD-LanguagesPopulationData.json \
     > ./data/languages-gallery.json
