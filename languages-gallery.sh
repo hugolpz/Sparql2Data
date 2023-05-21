6f232daab78ff4394d16f30d7c38ae726cacb613
Lopez Hugo <hugo.lpz@gmail.com>
2023-05-21 14:40:55 +0200

    Update languages-gallery.sh

--- languages-gallery.sh
+++ languages-gallery.sh
@@ -1,3 +1,3 @@
 jq -s 'add | group_by(.wikidata) | map(add) | map(select(.records > 0))' \
-    ./data/LL-LanguagesGenderData.json ./data/LL-LanguagesActive.json ./data/LL-LanguagesRecordsData.json ./data/WD-LanguagesPopulationData.json \
+    ./data/LL-LanguagesSpeakersData.json ./data/LL-LanguagesGenderData.json ./data/LL-LanguagesActive.json ./data/LL-LanguagesRecordsData.json ./data/WD-LanguagesPopulationData.json \
     > ./data/languages-gallery.json
