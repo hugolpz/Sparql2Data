0f655dfaf395940898b8c70c6406e490692a03be
Lopez Hugo <hugo.lpz@gmail.com>
2023-06-02 19:00:26 +0200

    Update and rename languages-gallery.sh to consolidator.sh

--- consolidator.sh
+++ consolidator.sh
@@ -1,3 +1,4 @@
+# Consolidated statistics for all languages -> `data/languages-gallery.json`
 jq --slurp --sort-keys 'add | group_by(.wikidata) | map(add) | map(select(.records > 0)) | sort_by((.records | tonumber) // 0) | reverse' \
     ./data/LL-LanguagesSpeakersData.json ./data/LL-LanguagesGenderData.json ./data/LL-LanguagesActive.json ./data/LL-LanguagesRecordsData.json ./data/WD-LanguagesPopulationData.json \
     > ./data/languages-gallery.json
